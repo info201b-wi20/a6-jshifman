e261eacb42f8e267b6f6b5f24a4202b036a41cad
Joseph Shifman <jshifman@uw.edu>
2020-03-07 22:43:09 -0800

    completed assignment

---
+++ app_server.R
@@ -0,0 +1,41 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+
+server <- function(input, output) {
+  output$scatter <- renderPlotly({
+    title <- paste0("Midwest Dataset Scatterplot")
+    p <- ggplot(midwest) +
+      geom_point(
+        mapping = aes_string(x = input$xaxis, y = input$yaxis),
+        size = input$size
+      ) +
+      labs(title = title)
+    p
+  })
+
+  output$bar <- renderPlotly({
+    population <- input$population
+    bar_data <- midwest %>%
+      filter(poptotal >= population) %>%
+      group_by(state) %>%
+      summarise(
+        avg_percwhite = mean(percwhite),
+        avg_percblack = mean(percblack),
+        avg_percamerindan = mean(percamerindan),
+        avg_percasian = mean(percasian),
+        avg_percother = mean(percother),
+        avg_percbelowpoverty = mean(percbelowpoverty),
+        avg_percchildbelowpovert = mean(percchildbelowpovert)
+      )
+    title <- paste("Metro Area Demographics")
+    p <- ggplot(bar_data) +
+      geom_col(
+        mapping = aes(x = state, y = !!as.name(input$yaxis2)),
+        fill = input$color
+      ) +
+      labs(title = title)
+    p
+  })
+}
---
+++ app_ui.R
@@ -0,0 +1,92 @@
+library(shiny)
+library(ggplot2)
+library(plotly)
+
+selected_values1 <- colnames(midwest)
+selected_values2 <- c(
+  "avg_percwhite",
+  "avg_percblack",
+  "avg_percamerindan",
+  "avg_percasian",
+  "avg_percother",
+  "avg_percbelowpoverty",
+  "avg_percchildbelowpovert"
+)
+
+x_input <- selectInput(
+  "xaxis",
+  label = "X-Axis Value",
+  choices = selected_values1,
+  selected = "poptotal"
+)
+
+y_input <- selectInput(
+  "yaxis",
+  label = "Y-Axis Value",
+  choices = selected_values1,
+  selected = "percwhite"
+)
+
+
+y_input2 <- selectInput(
+  "yaxis2",
+  label = "Y-Axis Value",
+  choices = selected_values2,
+  selected = "avg_percbelowpoverty"
+)
+
+size_input <- sliderInput(
+  "size",
+  label = "Size of point", min = 1, max = 10, value = 5
+)
+
+color_input <- selectInput(
+  "color",
+  label = "Color",
+  choices = list("Red" = "red", "Blue" = "blue", "Green" = "green")
+)
+
+population_input <- sliderInput(
+  "population",
+  label = "Minimum Population",
+  min = 1700,
+  max = 5000000,
+  value = 200000,
+  step = 100000,
+)
+
+tab1 <- tabPanel(
+  "Scatter plot",
+  h1("A6 Homework"),
+  sidebarLayout(
+    sidebarPanel(
+      x_input,
+      y_input,
+      size_input
+    ),
+    mainPanel(
+      plotlyOutput("scatter")
+    )
+  )
+)
+
+tab2 <- tabPanel(
+  "Bar Graph",
+  h1("A6 Homework"),
+  sidebarLayout(
+    sidebarPanel(
+      y_input2,
+      population_input,
+      color_input,
+    ),
+    mainPanel(
+      plotlyOutput("bar")
+    )
+  )
+)
+
+ui <- navbarPage(
+  "Graphs",
+  tab1,
+  tab2
+)
